2df6416171d7728a4776e497fec5d16d1e00d446
John <example@gmail.com>
2022-11-27 22:18:11 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,120 @@
+/* "use strict";
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number =10;
+console.log(number);
+
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+
+console.log(obj); */
+
+/* "use strict"
+
+let number = 4.6;
+
+console.log(-4/0);
+console.log('string' * 9)
+
+const persone = "5";
+
+const bool = false;
+
+console.log(something);
+
+let und;
+console.log(und);
+
+const obj = {
+    name: "John",
+    age:25,
+    isMarried: false
+}; */
+
+/* console.log(obj .name) */
+/* console.log(obj["name"]);
+
+            0           1           2    3          4
+let arr = ["plum.png", "orange.jpg", 6, "apple.bmp", {} ];
+console.log(arr[1]); */
+
+
+/* const arr = ['a', 'b', 'c'];
+const arrObj = {
+    a:'a',
+    '1':'b',
+    2:'c',
+    abc: {
+        df: [{},{}],
+        def: {
+
+        }
+    }
+};
+const b = 'b'; */
+
+/* arrObj.b = '1234'; */
+/* arrObj['b'] = {};
+
+console.log(arrObj.b);
+console.log(arrObj ['b']); */
+
+/* const obj = {a:1, b:2}; */
+/* const obj = {
+    Anna: 500,
+    'Alice': 800
+}; */
+
+
+
+/* alert('Hello'); */
+
+/* const result = confirm("Are you here?");
+console.log(result); */
+
+/* const answer = +prompt("Вам есть 18?", "18");
+console.log(answer + 5); */
+/* Информация от пользователя всегда будет приходить в виде строк */
+
+
+/* const answers = [];
+
+answers[0] = prompt ('Как ваше имя?', '');
+answers[1] = prompt ('Как ваша фамилия?', '');
+answers[0] = prompt ('Сколько вам лет?', '');
+
+console.log(typeof(answers));
+console.log(typeof(null)); */
+
+
+/* const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Ivan';
+
+alert(`Привет, ${user}`); */
+
+/* let incr = 10,
+    decr = 10; */
+
+/* ++incr;
+--decr; */
+
+console.log(++incr);
+console.log(--decr);
+
+/* console.log(5%2);
+
+console.log(2 + 2 * 2 !== `6`);
+
+
+const isChecked = false;
+      isClose = false;
+ 
+console.log(isChecked || !isClose); */
